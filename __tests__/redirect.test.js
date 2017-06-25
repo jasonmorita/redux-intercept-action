4b4a10d8f21f289ab2a8097d43dcabf5a972c508
Jason Morita <jason.morita@globality.com>
2017-06-24 21:11:07 -0700

    Initial commit

---
+++ __tests__/redirect.test.js
@@ -0,0 +1,66 @@
+import identity from 'lodash/identity';
+import redirectActions from '../';
+
+describe('Redirect action middlware', () => {
+    let redirect;
+    let actionCreatorA;
+    let actionCreatorB;
+    let config;
+    const store = {
+        dispatch: action => action,
+    };
+
+    beforeEach(() => {
+        actionCreatorA = jest.fn();
+        actionCreatorB = jest.fn();
+
+        config = {
+            MEMBERSHIP_EVENT_MEMBERSHIP_ADDED_READY: [actionCreatorA, actionCreatorB],
+            MEMBERSHIP_EVENT_PARENT_UPDATED_READY: actionCreatorA,
+        };
+
+        redirect = redirectActions(config);
+    });
+
+    describe('Dispatch an array of actions present in the redirect config', () => {
+        it('should dispatch an array of actions', () => {
+            redirect(store)(identity)({
+                type: 'MEMBERSHIP_EVENT_MEMBERSHIP_ADDED_READY',
+                payload: {
+                    id: 'abc-def-ghi',
+                },
+            });
+
+            expect(actionCreatorA.mock.calls.length).toBe(1);
+            expect(actionCreatorB.mock.calls.length).toBe(1);
+        });
+    });
+
+    describe('Dispatch a single action present in the redirect config', () => {
+        it('should dispatch single action', () => {
+            redirect(store)(identity)({
+                type: 'MEMBERSHIP_EVENT_PARENT_UPDATED_READY',
+                payload: {
+                    id: 'abc-def-ghi',
+                },
+            });
+
+            expect(actionCreatorA.mock.calls.length).toBe(1);
+            expect(actionCreatorB.mock.calls.length).toBe(0);
+        });
+    });
+
+    describe('Skip dispatch because no action type found in config', () => {
+        it('should dispatch an action we do not know about', () => {
+            redirect(store)(identity)({
+                type: 'ACCESS_CONTROL_SET_STATUS_TYPES',
+                payload: {
+                    id: 'abc-def-ghi',
+                },
+            });
+
+            expect(actionCreatorA.mock.calls.length).toBe(0);
+            expect(actionCreatorB.mock.calls.length).toBe(0);
+        });
+    });
+});
